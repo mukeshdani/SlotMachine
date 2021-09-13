a06b0eeaaba32a26d32997576df6b41c44160ba1
mukeshdani <mukeshdani00@gmail.com>
2021-09-13 22:12:23 +0530

    Created a Heading

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const App = ()=>{
+return <>
+          <h1 className='heading_style'>  🎰 Welcome to {" "} <span style={{fontWeight:'bold'}}>Slot machine </span> 🎰 </h1>
+      </>
+      }
+ 
+
+export default App;
